44dc8308e709c81531dff184a0fbdac8cf92c0f9
Abdelsalam Elramady <abdoelramady4@gmail.com>
2019-11-16 00:30:01 +0200

    Add Book class

---
+++ Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Library.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        [Required]
+        public string name { get; set; }
+        public int copy { get; set; }
+    }
+}
