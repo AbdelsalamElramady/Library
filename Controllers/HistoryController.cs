078e30041c05513d01d58361a9607bc2006577cc
Abdelsalam Elramady <abdoelramady4@gmail.com>
2019-11-16 06:36:20 +0200

    Add History controller and it's action methodes and views

---
+++ Controllers/HistoryController.cs
@@ -0,0 +1,22 @@
+﻿using Library.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Library.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private LibraryContext db = new LibraryContext();
+
+        // GET: History
+        public ActionResult Index()
+        {
+            ViewBag.clients = db.Books.ToList();
+            ViewBag.clients = db.Clients.ToList();
+            return View(db.Borrows.Where(b => b.returnDate != null).ToList());
+        }
+    }
+}
